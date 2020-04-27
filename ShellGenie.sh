fc3cea9ac4c4a4a9881ca55adddea2c57261a6ec
Shinaz Bin Shajahan <42292356+ShinazBinShajahan@users.noreply.github.com>
2020-04-27 09:34:29 +0000

    Update ShellGenie.sh

--- ShellGenie.sh
+++ ShellGenie.sh
@@ -1,4 +1,7 @@
 pip install random
-pkg install figlet
+if [ $(dpkg-query -W -f='${Status}' figlet 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install figlet
+fi
 cd Assets
 python tr.py
