e18bbc01319e7bb87ec4f5268cbd3d3bf46b0700
Shinaz Bin Shajahan <42292356+ShinazBinShajahan@users.noreply.github.com>
2020-04-27 09:11:04 +0000

    Update fsh.sh

--- Assets/data/fsh/fsh.sh
+++ Assets/data/fsh/fsh.sh
@@ -1,5 +1,5 @@
 pkg install fish
-cp -f Assets/data/fsh/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
+cp -f data/fsh/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
 echo
 echo " Successfully Updated your Termux Shell , Open A new Session To See The Update !"
 echo 
