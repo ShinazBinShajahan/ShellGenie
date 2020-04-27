1218d2755fb2544c68917506162c11a828a6df3b
Shinaz Bin Shajahan <42292356+ShinazBinShajahan@users.noreply.github.com>
2020-04-27 09:11:23 +0000

    Update dflt.sh

--- Assets/data/dflt/dflt.sh
+++ Assets/data/dflt/dflt.sh
@@ -1,4 +1,4 @@
-cp -f Assets/data/dflt/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
+cp -f data/dflt/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
 echo
 echo " Successfully Updated your Termux Shell , Open A new Session To See The Update !"
 echo 
