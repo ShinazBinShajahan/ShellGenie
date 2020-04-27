07bae017b22b8ce2eb2ec07b07bdbb9936a1bdb4
Shinaz Bin Shajahan <42292356+ShinazBinShajahan@users.noreply.github.com>
2020-04-27 09:10:44 +0000

    Update prt.sh

--- Assets/data/prt/prt.sh
+++ Assets/data/prt/prt.sh
@@ -1,4 +1,4 @@
-cp -f Assets/data/prt/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
+cp -f data/prt/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
 echo
 echo " Successfully Updated your Termux Shell , Open A new Session To See The Update !"
 echo
